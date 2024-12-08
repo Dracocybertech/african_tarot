5869195b0fa5f43c4c07aaeb421d0cac7a6e9fcf
Dracocybertech <pointdujourkh@gmail.com>
2024-12-08 19:29:02 +0100

    WIP testDistributeCards

--- srctest/game/TestGame.java
+++ srctest/game/TestGame.java
@@ -2,19 +2,37 @@ package srctest.game;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import deck.RemovingTooManyCards;
+import game.BadNumberOfPlayersException;
 import game.Game;
+import game.NotEnoughCardsInDeckException;
 import people.Player;
+import people.PlayerNameTooLongException;
+import people.TooManyCardsException;
 
 public class TestGame {
 
     private final InputStream originalSystemIn = System.in;
     private ByteArrayInputStream testIn;
     Game game;
+    ArrayList<Player> players;
+    @Before
+    public void beforeTest() throws PlayerNameTooLongException, BadNumberOfPlayersException{
+        game = new Game();
+        players = new ArrayList<>(Game.NUMBER_PLAYERS);
+        players.add(new Player("Player1"));
+        players.add(new Player("Player2"));
+        players.add(new Player("Player3"));
+        players.add(new Player("Player4"));
+        game.setPlayers(players);
+    }
 
     @AfterEach
     public void restoreSystemIn() {
@@ -47,5 +65,18 @@ public class TestGame {
         game = new Game();
         game.createPlayers();
         Assert.assertEquals(game.getPlayers().size(), Game.NUMBER_PLAYERS);
+    }
+
+    @Test
+    public void testDistributeCards() throws NotEnoughCardsInDeckException, TooManyCardsException, RemovingTooManyCards{
+        // Simulate user input "Hello World"
+        String simulatedInput = "Player1 Player2 Player3 Player4";
+        testIn = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(testIn);
+        game = new Game();
+        game.createPlayers();
+        int deckSize = game.getDeckSize();
+        //int maxCardsDistributable = deckSize / game.getPl
+        game.distributeCards(5);
     }
 }
