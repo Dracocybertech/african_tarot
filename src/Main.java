1e9917a93522be3d014a42bc9a891cd5d5029cb9
Dracocybertech <pointdujourkh@gmail.com>
2024-12-18 02:38:23 +0100

    +Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-import deck.Card;
-import deck.Deck;
+import java.util.Scanner;
+
+import game.Game;
 
 public class Main {
-    
+        
     public static void main (String[] arg){
+        Game game = new Game();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Start of the program.");
+        game.start();
+        scanner.close();
     }
 }
